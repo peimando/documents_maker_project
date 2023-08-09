b8ad845d7656bf17b6fe4ee1b0adb450fb7631d0
Gabriela Barrera <g.barrera.angel@gmail.com>
2023-08-09 16:59:46 -0400

    - demo inline

--- docu/website/views/add_ordinario.py
+++ docu/website/views/add_ordinario.py
@@ -7,80 +7,133 @@ from ordinario.models import Ordinario
 from website.forms import AddOrdinarioForm, OrdinarioFormSet
 
 
-class AddOrdinario(CreateView):
+### ORIGINAL
+
+# class AddOrdinario(CreateView):
+
+#     form_class = AddOrdinarioForm
+
+#     template_name = 'website/ordinario/add_edit_ordinario_inline.html'
+
+#     def get_context_data(self, **kwargs):
+ 
+#         context = super(AddOrdinario, self).get_context_data(**kwargs)
+#         context['ordinario_distribucion_externa_formset'] = OrdinarioFormSet()
+#         context['title'] = f'Generar Ordinario'
+#         context['action'] = 'create'
+ 
+#         return context
+
+#     def post(self, request, *args, **kwargs):
+
+#         self.object = None
+
+#         form_class = self.get_form_class()
+
+#         form = self.get_form()
+
+#         ordinario_distribucion_externa_formset = OrdinarioFormSet(self.request.POST)
+
+#         if form.is_valid() and ordinario_distribucion_externa_formset.is_valid():
+
+#             return self.form_valid(form, ordinario_distribucion_externa_formset)
+        
+#         else:
+            
+#             return self.form_invalid(form, ordinario_distribucion_externa_formset)
+        
+#     def form_valid(self, form, ordinario_distribucion_externa_formset):
+
+#         self.object = form.save(commit=False)
+
+#         # Saving DistribucionesExternas Instance
+#         distribuciones_externas = ordinario_distribucion_externa_formset.save(commit=False)
+
+#         for distribucion_externa in distribuciones_externas:
+
+#             distribucion_externa.ordinario = self.object
+
+#             distribucion_externa.save(commit=False)
+
+#             # 
+
+#             distribucion_externa.save()
+
+#         self.object.save()
+
+#         messages.add_message(
+#             self.request,
+#             messages.SUCCESS,
+#             f"El ordinario '{self.object.materia}' se ha generado correctamente."
+#         )
+
+#         return super().form_valid(form)
+
+#     def form_invalid(self, form, ordinario_distribucion_externa_formset):
+
+#         return self.render_to_response(
+#             self.get_context_data(
+#                 form=form,
+#                 ordinario_distribucion_externa_formset= ordinario_distribucion_externa_formset
+#             )
+#         )
+    
+#     def get_success_url(self) -> str:
+        
+#         return reverse_lazy(
+#             'website:detail_ordinario',
+#             kwargs={
+#                 'slug': self.object.slug,
+#             }
+#         )
+
+
+class OrdinarioInline():
 
     form_class = AddOrdinarioForm
 
-    template_name = 'website/ordinario/add_edit_ordinario.html'
+    model = Ordinario
 
-    def get_context_data(self, **kwargs):
- 
-        context = super(AddOrdinario, self).get_context_data(**kwargs)
-        context['ordinario_distribucion_externa_formset'] = OrdinarioFormSet()
-        context['title'] = f'Generar Ordinario'
-        context['action'] = 'create'
- 
-        return context
+    template_name = 'website/add_edit_ordinario_inline.html'
 
-    def post(self, request, *args, **kwargs):
+    def form_valid(self, form):
 
-        self.object = None
+        named_formsets = self.get_named_formsets()
 
-        form_class = self.get_form_class()
+        if not all( (f.is_valid() for f in named_formsets.values())):
 
-        form = self.get_form()
+            return self.render_to_response(self.get_context_data(form=form))
 
-        ordinario_distribucion_externa_formset = OrdinarioFormSet(self.request.POST)
+        self.object = form.save()
 
-        if form.is_valid() and ordinario_distribucion_externa_formset.is_valid():
+        for name, formset in named_formsets.items():
 
-            return self.form_valid(form, ordinario_distribucion_externa_formset)
-        
-        else:
+            formset_save_func = getattr(self, f'formset_{0}_valid', None)
+
+            if formset_save_func is not None:
+
+                formset_save_func(formset)
+
+            else:
+
+                formset.save()
+
+            return redirect('website:list_ordinarios')
             
-            return self.form_invalid(form, ordinario_distribucion_externa_formset)
-        
-    def form_valid(self, form, ordinario_distribucion_externa_formset):
+    def formset_distribucion_externa_valid(self, formset):
 
-        self.object = form.save(commit=False)
+        """
+        Hook for custom formset saving... useful if you have multiple formsets
+        """
 
-        # Saving DistribucionesExternas Instance
-        distribuciones_externas = ordinario_distribucion_externa_formset.save(commit=False)
+        distribuciones_externas_formset = formset.save(commit=False)
 
-        for distribucion_externa in distribuciones_externas:
+        for obj in formset.deleted_objects:
+            
+            obj.delete()
 
-            distribucion_externa.ordinario = self.object
+        for distribucion in distribuciones_externas_formset:
 
-            distribucion_externa.save(commit=False)
+            distribucion.ordinario = self.object
+            distribucion.save()
 
-            # 
-
-            distribucion_externa.save()
-
-        self.object.save()
-
-        messages.add_message(
-            self.request,
-            messages.SUCCESS,
-            f"El ordinario '{self.object.materia}' se ha generado correctamente."
-        )
-
-        return super().form_valid(form)
-
-    def form_invalid(self, form, ordinario_distribucion_externa_formset):
-
-        return self.render_to_response(
-            self.get_context_data(
-                form=form,
-                ordinario_distribucion_externa_formset= ordinario_distribucion_externa_formset
-            )
-        )
-    
-    def get_success_url(self) -> str:
-        
-        return reverse_lazy(
-            'website:detail_ordinario',
-            kwargs={
-                'slug': self.object.slug,
-            }
-        )
